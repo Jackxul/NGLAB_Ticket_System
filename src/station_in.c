946404001e058f196a7866669598a3f5250939c2
Jack Xul <xyz1357997531@gmail.com>
2023-04-10 23:03:27 +0800

    Commit

--- src/station_in.c
+++ src/station_in.c
@@ -2,8 +2,10 @@
 //  Created by  on 2012-11-13.
 #include "station.h"
 void station_in(){
-	
-
+//check account remain >= 0	
+//record station color
+//record station number
+//record time
 
 
 
--- src/station_out.c
+++ src/station_out.c
@@ -1,7 +1,10 @@
 //station_out.c
 #include "station.h"
 void station_out(){
-
+//record station color
+//record station number
+//record time
+//
 
 
 
