cb1bafd4a9d51b98fd2b47deffab7bcc54bc55e3
Jack Xul <xyz1357997531@gmail.com>
2023-04-15 00:36:30 +0800

    add red and blue line station array

--- src/select.c
+++ src/select.c
@@ -4,6 +4,65 @@
 #include "select.h"
 void selection(){
 	char *st_info[]={"BR" , "BL" , "G" , "Y" , "R"};
+	char *BR[] = {"BR1" , "BR2" , "BR3" , "BR4" , "BR5"};
+	const char *BL[] = {
+		"BL1-Dingpu" ,
+		"BL2-Yongning" ,
+		"BL3-Tucheng" ,
+		"BL4-Haishan" ,
+		"BL5-Far Eastern Hospital" ,
+		"BL6-Fuzhong" ,
+		"BL7-Banqiao" ,
+		"BL8-Xinpu" ,
+		"BL9-Jiangzicui" ,
+		"BL10-Longshan Temple" ,
+		"BL11-Ximen" ,
+		"BL12-Taipei Main Station" ,
+		"BL13-Shandao Temple" ,
+		"BL14-Zhongxiao Xinsheng" ,
+		"BL15-Zhongxiao Fuxing" ,
+		"BL16-Zhongxiao Dunhua" ,
+		"BL17-Sun Yat-Sen Memorial Hall" ,
+		"BL18-Taipei City Hall" ,
+		"BL18-Taipei City Hall" ,
+		"BL19-Yongchun" ,
+		"BL20-Houshanpi" ,
+		"BL21-Kunyang" ,
+		"BL22-Nangang" ,
+		"BL23-Taipei Nangang Exhibition Center"
+	};
+	char *G[] = {"G1" , "G2" , "G3" , "G4" , "G5"};
+	char *Y[] = {"Y1" , "Y2" , "Y3" , "Y4" , "Y5"};
+	const char* R[] = {
+  		"R2-Xiangshan",
+  		"R3-Taipei 101 World Trade Center",
+ 	 	"R4-Xinyi Anhe",
+  		"R5-Daan",
+  		"R6-Daan Park",
+  		"R7-Dongmen",
+  		"R8-Chiang Kai-Shek Memorial Hall",
+  		"R9-NTU Hospital",
+  		"R10-Taipei Main Station",
+  		"R11-Zhonshan",
+  		"R12-Shuanglian",
+  		"R13-Minquan West Road",
+  		"R14-Yuanshan",
+  		"R15-Jiantan",
+  		"R16-Shilin",
+  		"R17-Zhishan",
+  		"R18-Mingde",
+  		"R19-Shipai",
+  		"R20-Qilian",
+  		"R21-Qiyan",
+  		"R22-Beitou",
+  		"R23-Fuxinggang",
+  		"R24-Zhongyi",
+  		"R25-XGuandu",
+  		"R26-Zhuwei",
+  		"R27-YHongshulin",
+  		"R28-Tamsui"
+	};
+
 	int st_size = sizeof(st_info)/sizeof(st_info[0]);
 	int select_item = 0;
 	bool flag = true;
